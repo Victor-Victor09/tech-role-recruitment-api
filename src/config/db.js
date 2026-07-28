5422d7878e45d2edbd960bd4c9d67e61489b74c8
Victor Olusegun <teeveeolusegun@gmail.com>
2026-07-28 17:21:26 +0100

    feat: configure postgresql database connection using sequelize

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,24 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        logging: console.log,
+    }
+);
+
+export default sequelize;
+
+
+
+
 /**
  * src/config/db.js
  * ------------------------------------------------------------
