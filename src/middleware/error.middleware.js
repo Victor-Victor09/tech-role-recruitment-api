1dbbfe772307030cde7ef3f10d1dbc8b587ad124
Victor Olusegun <teeveeolusegun@gmail.com>
2026-07-30 04:31:40 +0100

    feat(middleware): implement centralized error handler

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -21,3 +21,14 @@
  */
 
 // TODO: implement
+export function errorMiddleware(err, req, res, next) {
+    const statusCode = err.statusCode || 500;
+    const message = err.isOperational ? err.message : "Something went wrong.";
+
+    console.error(err); // Log the error for debugging (optional)
+
+    res.status(statusCode).json({
+        status: "error",
+        message,
+    })
+}
