07de786dabb06469901d3d6990d87d0f548983f4
Victor Olusegun <teeveeolusegun@gmail.com>
2026-08-05 03:42:33 +0100

    feat: add catchAsync async handler wrapper

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,5 +1,10 @@
-export const catchAsync = (fn) => (req, res, next) => {
+export default (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 };
 
 // This utility function is used to wrap asynchronous route handlers in Express.js.
+
+// const catchAsync = (fn) => (req, res, next) => {
+//   Promise.resolve(fn(req, res, next)).catch(next);
+// };
+// export default catchAsync;
