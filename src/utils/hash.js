3a29cb22ee9f2983a8205c8cef509ff18f539767
Victor Olusegun <teeveeolusegun@gmail.com>
2026-08-05 03:42:34 +0100

    feat: add bcrypt password hashing wrappers

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -6,5 +6,5 @@ import bcrypt from "bcrypt";
 // Number of rounds to use when hashing a password.
 // 10 is the default for bcrypt.
 const SALT_ROUNDS = 10;
- export const hashPassword = (plain) => bcrypt.hash(plain, SALT_ROUNDS);
- export const comparePassword = (plain, hash) => bcrypt.compare(plain, hash);
+export const hashPassword = (plain) => bcrypt.hash(plain, SALT_ROUNDS);
+export const comparePassword = (plain, hash) => bcrypt.compare(plain, hash);
