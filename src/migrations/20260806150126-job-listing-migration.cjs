704d85b000d503c5d9d9ee9b71de73005cd16268
Victor Olusegun <teeveeolusegun@gmail.com>
2026-08-12 19:00:18 +0100

    fix: correct typo in job listing migration rollback; jobsListing to jobListing

--- src/migrations/20260806150126-job-listing-migration.cjs
+++ src/migrations/20260806150126-job-listing-migration.cjs
@@ -62,6 +62,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('josbListings');
+    await queryInterface.dropTable('jobListings');
   }
 };
