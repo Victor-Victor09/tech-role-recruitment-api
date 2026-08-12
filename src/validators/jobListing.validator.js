745f6e538be82aed90b16d845bec64d031fc1904
Victor Olusegun <teeveeolusegun@gmail.com>
2026-08-13 00:20:51 +0100

    fix: correct jobListing validator exports and workPreference values

--- src/validators/jobListing.validator.js
+++ src/validators/jobListing.validator.js
@@ -1,11 +1,28 @@
 import { body } from "express-validator";
+import { WORK_PREFERENCE } from "../constants/jobTypes.js";
+
+const workPreferenceValues = Object.values(WORK_PREFERENCE);
 
 export const createJobListingValidator = [
     body("title")
         .notEmpty().withMessage("Job title is required"),
     body("description")
         .notEmpty().withMessage("Job description is required"),
+    body("techRole")
+        .notEmpty().withMessage("Tech role is required"),
     body("workPreference")
         .notEmpty().withMessage("Work preference is required")
-        .isIn(["remote", "hybrid", "onsite"]).withMessage("Work preference must be one of: remote, hybrid, onsite"),
+        .isIn(workPreferenceValues).withMessage("Work preference is not valid"),
+]
+
+export const updateJobListingValidator = [
+    body("title")
+        .optional().notEmpty().withMessage("Job title cannot be empty"),
+    body("description")
+        .optional().notEmpty().withMessage("Job description cannot be empty"),
+    body("techRole")
+        .optional().notEmpty().withMessage("Tech role cannot be empty"),
+    body("workPreference")
+        .optional().notEmpty().withMessage("Work preference cannot be empty")
+        .isIn(workPreferenceValues).withMessage("Work preference is not valid"),
 ]
