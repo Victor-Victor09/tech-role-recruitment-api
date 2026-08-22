808c7928c89f0a8ba11a236bc3236860d1fe27e8
Victor Olusegun <teeveeolusegun@gmail.com>
2026-08-22 01:45:54 +0100

    fix: fix little typo in ApiError message

--- src/middleware/role.middleware.js
+++ src/middleware/role.middleware.js
@@ -16,7 +16,7 @@ import ApiError from "../utils/ApiError.js";
 export const requireRole = (role) => (req, res, next) => {
 
     if (!req.user || req.user.role !== role){
-        return next( new ApiError("Yo do not have permission to perform this action", 403));
+        return next( new ApiError("You do not have permission to perform this action", 403));
     }
     next();
 };
